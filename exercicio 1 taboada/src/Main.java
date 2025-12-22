5fd714665eb92c6e1a0ccb5dbadc4ab3b8449ac7
Luiz Felipe Carvalho <86491103+luizcarvalho20@users.noreply.github.com>
2025-12-22 13:38:59 -0300

    Exercicio de java bootcamp dio

---
+++ exercicio 1 taboada/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para ver a tabuada: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("\nTabuada do " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        scanner.close();
+    }
+}
